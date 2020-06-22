6ee7d38cda2263956c800842a516244957518cd1
Abdullah Zaidi <66746291+AbdullahAZaidi@users.noreply.github.com>
2020-06-22 08:59:29 -0700

    Create Minimum_Coins.py

---
+++ Minimum_Coins.py
@@ -0,0 +1,24 @@
+'''
+
+Find the minimum number of coins needed to make up the amount
+
+'''
+
+
+def min_coins(cents,coins):
+    if (cents ==0):
+        return 0
+    
+    coins.sort(reverse = True)
+    coins_used= []
+    count = 0
+    for coin in coins:
+        while (coin <= cents):
+            count+=1
+            coins_used.append(coin)
+            cents = cents-coin
+            
+    return (count)
+
+coins =[2,1]
+print(min_coins(7,coins))
